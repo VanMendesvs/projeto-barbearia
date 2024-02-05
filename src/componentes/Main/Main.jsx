1a35f84ad1f26a67ea511c59c997c954f92152e7
VanMendesvs <v_mendesv@hotmail.com>
2024-02-05 12:33:15 -0300

    Atualização hook useState

--- src/componentes/Main/Main.jsx
+++ src/componentes/Main/Main.jsx
@@ -1,34 +1,46 @@
-import React from "react"
-import Cortes from "../../componentes/Main/careca.jpg"
+import React, { useState } from "react" //chamando o hook useState
+import Cortes from "../../componentes/Main/careca.jpg" //importando imagens
 import Barba from "../../componentes/Main/barba.jpeg"
 import Infantil from "../../componentes/Main/corteinfantil.jpg"
+import Carrinho from "../../componentes/Main/carrinho.png"
 
 function Main() {
+
+        const [numero, setNumero] = useState (0)
+        //const [o objeto, função para mudar o estado o objeto] =useState(valor incial)
+
+        const Adicionar = () => {
+                   setNumero(numero + 1)
+            }
+        
+
         return(
             <main>
-                <h1>SERVIÇOS</h1>
-                <div>
-                <section className="card1">
+               
+            
+                <div className="card1">
                 <img className="cortes" src={Cortes} alt=""/> 
                 <h3>Cortes</h3>
-                <h3>R$ 50,00</h3>
-                <button>AGENDE AGORA</button>
-                </section>
+                <h3>50,00</h3>
+                <button onclick={Adicionar}>AGENDAR<img src={Carrinho}alt=""/></button>
+                <h3> {numero} </h3>
+                </div>  
 
-                <section className="card2">
+                <div className="card2">
                 <img className="barba" src={Barba} alt="Foto de Homem com Barba"/>
                 <h3>Barba e Bigode</h3>
                 <h3>R$ 40,00</h3>
-                <button>AGENDE AGORA</button>
-                </section>
+                <img src={Carrinho} alt="" onclick={Adicionar}/>
+                <h3> {numero} </h3>
+                </div>
 
-                <section className="card3">
+                <div className="card3">
                     <img className="corteinfantil"src={Infantil} alt="Menino cortando cabelo"/>
                     <h3>Corte Infantil</h3>
                     <h3>R$ 50,00</h3>
                     <button>AGENDE AGORA</button>
-                </section>
                 </div>
+                
             </main>
         )
 
